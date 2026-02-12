3f4b41a6686618e6e49bd95f0a647ac2e836b9d5
Maithiliburghate <maithiliburghate2003@gmail.com>
2026-02-12 23:27:31 +0530

    Updated complete project

--- poc-ui/src/app/app.component.ts
+++ poc-ui/src/app/app.component.ts
@@ -1,9 +1,36 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports: [CommonModule, FormsModule, HttpClientModule],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
-export class AppComponent {}
+export class AppComponent {
+
+  user = {
+    firstName: '',
+    lastName: '',
+    pincode: ''
+  };
+
+  users: any[] = [];
+
+  save() {
+    console.log(this.user);
+
+    this.users.push({ ...this.user });
+
+    // reset form
+    this.user = {
+      firstName: '',
+      lastName: '',
+      pincode: ''
+    };
+  }
+
+}
